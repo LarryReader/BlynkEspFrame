0386f43758d1982b6c035693cea8dee7e7607472
LarryReader <larryreader@gmail.com>
2018-11-10 09:25:44 -0500

    Update auth.h

--- lib/auth/auth.h
+++ lib/auth/auth.h
@@ -1,7 +1,7 @@
 //Configuration and Credentials
-char auth[] = "ad948bfe5868470a9dffde4c3c747332"; // BLYNK APP - MinimalPlanter
+char auth[] = "ad948----7332"; // BLYNK APP - MinimalPlanter
 
 //WiFi
 //St Lukes
 char ssid[] = "D1B436";
-char pass[] = "55539777";
+char pass[] = "55---77";
