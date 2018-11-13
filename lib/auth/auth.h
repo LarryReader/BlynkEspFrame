ad1e1bd7bc358a984bee9f6ab735a83107beafe6
LarryReader <larryreader@gmail.com>
2018-11-13 18:17:38 -0500

    Update auth.h

--- lib/auth/auth.h
+++ lib/auth/auth.h
@@ -1,20 +1,20 @@
 //Configuration and Credentials
-char auth[] = "ad948bfe5868470a9dffde4c3c747332"; // BLYNK APP - MinimalPlanter
+char auth[] = "ad94--7332"; // BLYNK APP - MinimalPlanter
 
 //WiFi
 
 //Phat lab
 char ssid[] = "Fat_Cat_Fablab";
-char pass[] = "m4k3rb0t";
+char pass[] = "m4--0t";
 
 // WiFi credentials.
 // Set password to "" for open networks.
 
 // char ssid[] = "MySpectrumWiFi7c-2G";
-// char pass[] = "chillyrabbit361";
+// char pass[] = "chi--361";
 
 //char ssid[] = "Fat_Cat_Fablab"; 
-//char pass[] = "m4k3rb0t";
+//char pass[] = "m4--0t";
 
 //char ssid[] = "LRPixel";
 //char pass[] = "LRPixel222";
